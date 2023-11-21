181ade546c459bc984f4985d082613064637ac38
KimGyuWan <y5g5776@naver.com>
2023-11-21 22:06:59 +0900

    언어변경 실행식 초기작업 중

--- js/dataKo.js
+++ js/dataKo.js
@@ -69,28 +69,36 @@ const langKo = {
     h2: "GOLD MEMBER 골드멤버8가지혜택",
     i: [
       {
+        cls: "bi bi-plus",
+        span: "어린이날/크리스마스 연2회선물"
+      },
+      {
         cls: "bi-balloon-heart",
+        span: "반려견/반려묘 생일케이크 쿠폰"
       },
       {
-        cls: "bi-gift",
+        cls: "bi bi-gift",
+        span: "밥그릇+스쿱+턱받이 웰컴키트"
       },
       {
-        cls: "bi-box2-heart",
+        cls: "bi bi-box2-heart",
+        span: "제품 10개 구매시 8,000원 쿠폰"
       },
       {
-        cls: "bi-ticket-perforated",
+        cls: "bi bi-ticket-perforated",
+        span: "가장맛있는시간 30일 언제나 무료배송"
       },
       {
-        cls: "bi-truck",
+        cls: "bi bi-envelope-check",
+        span: "생산과정 사진/영상 문자 발송 서비스"
       },
       {
-        cls: "bi-envelope-check",
+        cls: "bi bi-cash-coin",
+        span: "구매금액 5%적립"
       },
       {
-        cls: "bi-cash-coin",
-      },
-      {
-        cls: "bi-headset",
+        cls: "bi bi-headset",
+        span: "펫파트너스 CS핫라인 서비스"
       },
     ]
   },
@@ -389,4 +397,87 @@ const langKo = {
       },
     ]
   },
+  footer: {
+    box: [
+      {
+        h4: "COMPANY INFO",
+        p: "㈜ 하림펫푸드 | 대표 : 민동기 | 충청남도 공주시 정안면 정안농공단지길 32-111 | 사업자 등록 번호 : 240-81-00698 | 통신판매업 신고 제 2017-충남공주-0042호 | 개인정보관리책임자 : 조준형 | FAX : 02-6933-4402"
+      },
+      {
+        h4: "CUSTOMER CENTER",
+        p: "080-850-0085 | Open : AM 09:00 ~ PM 05:00 | Lunch : PM 12:00 ~ PM 01:00 | 토,일,공휴일 휴무"
+      }
+    ],
+    family: [
+      {
+        nm: "(주)하림",
+        href: "https://www.harim.com/"
+      },
+      {
+        nm: "하림몰",
+        href: "https://harimmall.com/"
+      },
+      {
+        nm: "선진",
+        href: "https://www.sj.co.kr/"
+      },
+      {
+        nm: "팜스코",
+        href: "https://www.hypork.com/"
+      },
+      {
+        nm: "주원산오리",
+        href: "https://www.orimart.co.kr/"
+      },
+      {
+        nm: "NS홈쇼핑",
+        href: "http://nsmall.com/"
+      },
+      {
+        nm: "맥시칸치킨",
+        href: "http://www.mexican.co.kr/"
+      },
+      {
+        nm: "디디치킨",
+        href: "http://www.ddck.co.kr/"
+      },
+    ],
+    tabicon: [
+      {
+        href: "",
+        span: "페이스북",
+      },
+      {
+        href: "",
+        span: "인스타그램",
+      },
+      {
+        href: "",
+        span: "유튜브",
+      },
+    ]
+  }
 }
+
+let topadTag = "";
+let gnbTag = "";
+let slidebannerTag = "";
+let linebannerTag = "";
+let sectionbrandTag = "";
+let sectionitemTag = "";
+let sectioninstagramTag = "";
+let footerTag = "";
+
+
+window.addEventListener("load", function () {
+  for (x of langKo) {
+    let obj = "";
+    obj += x;
+    console.log(obj)
+    for (y in langKo) {
+      topadTag += `<a href="${langKo.obj[y].href} class="${langKo.obj[y].cls}">${langKo.obj[y].nm}</a>`;
+    }
+  }
+
+  document.querySelector("#topbanner").innerHTML = topadTag;
+})
