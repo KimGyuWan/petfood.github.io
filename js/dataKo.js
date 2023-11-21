1273a6327b258ff30e13dac310fa3282389b514b
User -2023KFDWY\Administrator <flxk202122@naver.com>
2023-11-21 16:09:05 +0900

    footer css 29-30 줄 ul->div 로 수정, html 태그수정

---
+++ js/dataKo.js
@@ -0,0 +1,124 @@
+const langKo = {
+  // topAd: [
+  //   { 
+  //     nm: "하림펫푸드 제품 첫 구매 시 간식 100원 이벤트",
+  //     cls: "d-none",
+  //     link : ""
+  //   }
+  // ],
+  // gnb: [
+  //   {
+  //     nm: "event",
+  //     href: "#slide-banner",
+  //     cls: "text-dark  position-relative",
+  //     target : ""
+  //   },
+  //   {
+  //     nm: "event",
+  //     href: "#slide-banner",
+  //     cls: "",
+  //     target: ""
+  //   },
+  //   {
+  //     nm: "event",
+  //     href: "#slide-banner",
+  //     cls: "",
+  //     target: ""
+  //   },
+  //   {
+  //     nm: "event",
+  //     href: "#slide-banner",
+  //     cls: "",
+  //     target: ""
+  //   },
+  //   {
+  //     nm: "event",
+  //     href: "#slide-banner",
+  //     cls: "",
+  //     target: ""
+  //   }
+
+  // ],
+  slidebanner: [
+    {
+      h3: "반짝 배송 OPEN",
+      p: "오늘 주문, 반짝하면 새벽 도착!",
+      href: "",
+      src: "./img/yeonsoo/slide1.png",
+    },
+    {
+      h3: "설레는 봄 | 할인해 봄",
+      p: "봄맞이 최대 50% 할인 혜택 받으세요",
+      href: "",
+      src: "./img/yeonsoo/slide2.png",
+    },
+    {
+      h3: "신선함이 가득한 | 당일생산",
+      p: "하림펫푸드는 사료가 아니다 | 음식이다",
+      href: "",
+      src: "./img/yeonsoo/slide3.png",
+    },
+    {
+      h3: "댕냥이 | 동안 선발대회",
+      p: "#하림팻푸드 댕냥이 동안선발대회 | 반려동물 자랑도 하고 간식도 받고!",
+      href: "",
+      src: "./img/yeonsoo/slide4.png",
+    },
+    {
+      h3: "멍이냥이얌얌 | 리뷰 EVENT",
+      p: "하림펫푸드 제품 리뷰 작성시 | 추첨을 통해 간식 샘플 증정",
+      href: "",
+      src: "./img/yeonsoo/slide5.png",
+    },
+  ],
+
+  linebanner: {
+    h2: "GOLD MEMBER 골드멤버8가지혜택",
+    i: [ 
+      {
+        cls: "bi bi-plus",
+      },
+      {
+        cls: "bi-balloon-heart",
+      },
+      {
+        cls: "bi bi-gift",
+      },
+      {
+        cls: "bi bi-box2-heart",
+      },
+      {
+        cls: "bi bi-ticket-perforated",
+      },
+      {
+        cls: "bi bi-envelope-check",
+      },
+      {
+        cls: "bi bi-cash-coin",
+      },
+      {
+        cls: "bi bi-headset",
+      },
+    ]
+  },
+
+  footer: {
+    h2: "",
+    href: "",
+    src: "./img/yeonsoo/petfoodlogo.png",
+    info: [
+
+    ]
+  }
+  // sectionbrand : {
+  //   h2: "우리의 약속 | OUR COMMITMENT",
+  //   brandlist: [
+  //     {
+  //       src: "./img/dongmin/form1.gif",
+  //       h3: "하림펫푸드의 신선도는 | 냄새로 확인할 수 있습니다.",
+  //       p: "보통 사료는 나쁜 냄새가 많이 나지만  | 하림펫푸드는 생고기와 야채 등신선한 재료로만 만들어  | 재료 본연의 고소한 향을 느낄 수 있습니다."
+  //     }
+  //   ]
+
+  // }
+}
