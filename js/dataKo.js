34a5da0ec92701b5e74141f3796786cdc24aeead
User -2023KFDWY\Administrator <flxk202122@naver.com>
2023-11-21 16:49:23 +0900

    footer 데이터화 작업중

--- js/dataKo.js
+++ js/dataKo.js
@@ -1,44 +1,4 @@
 const langKo = {
-  // topAd: [
-  //   { 
-  //     nm: "하림펫푸드 제품 첫 구매 시 간식 100원 이벤트",
-  //     cls: "d-none",
-  //     link : ""
-  //   }
-  // ],
-  // gnb: [
-  //   {
-  //     nm: "event",
-  //     href: "#slide-banner",
-  //     cls: "text-dark  position-relative",
-  //     target : ""
-  //   },
-  //   {
-  //     nm: "event",
-  //     href: "#slide-banner",
-  //     cls: "",
-  //     target: ""
-  //   },
-  //   {
-  //     nm: "event",
-  //     href: "#slide-banner",
-  //     cls: "",
-  //     target: ""
-  //   },
-  //   {
-  //     nm: "event",
-  //     href: "#slide-banner",
-  //     cls: "",
-  //     target: ""
-  //   },
-  //   {
-  //     nm: "event",
-  //     href: "#slide-banner",
-  //     cls: "",
-  //     target: ""
-  //   }
-
-  // ],
   slidebanner: [
     {
       h3: "반짝 배송 OPEN",
@@ -74,51 +34,101 @@ const langKo = {
 
   linebanner: {
     h2: "GOLD MEMBER 골드멤버8가지혜택",
-    i: [ 
+    i: [
       {
         cls: "bi bi-plus",
+        span: "어린이날/크리스마스 연2회선물"
+
       },
       {
         cls: "bi-balloon-heart",
+        span: "반려견/반려묘 생일케이크 쿠폰"
       },
       {
         cls: "bi bi-gift",
+        span: "밥그릇+스쿱+턱받이 웰컴키트"
       },
       {
         cls: "bi bi-box2-heart",
+        span: "제품 10개 구매시 8,000원 쿠폰"
       },
       {
         cls: "bi bi-ticket-perforated",
+        span: "가장맛있는시간 30일 언제나 무료배송"
       },
       {
         cls: "bi bi-envelope-check",
+        span: "생산과정 사진/영상 문자 발송 서비스"
       },
       {
         cls: "bi bi-cash-coin",
+        span: "구매금액 5%적립"
       },
       {
         cls: "bi bi-headset",
+        span: "펫파트너스 CS핫라인 서비스"
       },
     ]
   },
 
   footer: {
-    h2: "",
-    href: "",
-    src: "./img/yeonsoo/petfoodlogo.png",
-    info: [
-
+    box: [
+      {
+        h4: "COMPANY INFO",
+        p: "㈜ 하림펫푸드 | 대표 : 민동기 | 충청남도 공주시 정안면 정안농공단지길 32-111 | 사업자 등록 번호 : 240-81-00698 | 통신판매업 신고 제 2017-충남공주-0042호 | 개인정보관리책임자 : 조준형 | FAX : 02-6933-4402"
+      },
+      {
+        h4: "CUSTOMER CENTER",
+        p: "080-850-0085 | Open : AM 09:00 ~ PM 05:00 | Lunch : PM 12:00 ~ PM 01:00 | 토,일,공휴일 휴무"
+      }
+    ],
+    family: [
+      {
+        nm: "(주)하림",
+        href: "https://www.harim.com/"
+      },
+      {
+        nm: "하림몰",
+        href: "https://harimmall.com/"
+      },
+      {
+        nm: "선진",
+        href: "https://www.sj.co.kr/"
+      },
+      {
+        nm: "팜스코",
+        href: "https://www.hypork.com/"
+      },
+      {
+        nm: "주원산오리",
+        href: "https://www.orimart.co.kr/"
+      },
+      {
+        nm: "NS홈쇼핑",
+        href: "http://nsmall.com/"
+      },
+      {
+        nm: "맥시칸치킨",
+        href: "http://www.mexican.co.kr/"
+      },
+      {
+        nm: "디디치킨",
+        href: "http://www.ddck.co.kr/"
+      },
+    ],
+    tabicon: [
+      {
+        href: "",
+        span: "페이스북",
+      },
+      {
+        href: "",
+        span: "인스타그램",
+      },
+      {
+        href: "",
+        span: "유튜브",
+      },
     ]
   }
-  // sectionbrand : {
-  //   h2: "우리의 약속 | OUR COMMITMENT",
-  //   brandlist: [
-  //     {
-  //       src: "./img/dongmin/form1.gif",
-  //       h3: "하림펫푸드의 신선도는 | 냄새로 확인할 수 있습니다.",
-  //       p: "보통 사료는 나쁜 냄새가 많이 나지만  | 하림펫푸드는 생고기와 야채 등신선한 재료로만 만들어  | 재료 본연의 고소한 향을 느낄 수 있습니다."
-  //     }
-  //   ]
-
-  // }
 }
